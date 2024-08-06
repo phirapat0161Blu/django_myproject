e26c24c45cd429409cd83bf45072a5882e89cdc4
phirapat0161Blu <ulucky5684@gmail.com>
2024-08-06 10:49:32 +0700

    new update django vercel

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+echo "Building the project..."
+python3.12 -m pip install -r requirements.txt
+
+echo "Collect Static..."
+python3.12 manage.py collectstatic --noinput --clear
