247768c75d4d0024cca8a8d70860919316000cf1
phirapat0161Blu <ulucky5684@gmail.com>
2024-08-13 11:49:38 +0700

    cardcolor update

--- webpage/urls.py
+++ webpage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",  views.indexPage, name="index"),
-    path("about/", views.aboutUs, name="about"),
-    path("contact/", views.contactUs),
+    path("about/", views.aboutPage, name="about"),
+    path("contact/", views.contactPage, name="contact"),
+    path("color/", views.cardColorPage, name="cardcolor"),
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
 def indexPage(request):
     return render(request, 'index.html')
 
-def aboutUs(request):
+
+def aboutPage(request):
     return render(request, 'about.html')
 
 
-def contactUs(request):
+def contactPage(request):
     return render(request, 'contact.html')
+
+
+def cardColorPage(request):
+    context = {
+        'color': 'all',
+    }
+
+    if request.method == "GET" and request.GET.get('color') != None:
+        context['color'] = request.GET.get('color')
+
+    return render(request, 'card_color.html', context)
